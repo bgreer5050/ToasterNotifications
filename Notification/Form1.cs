285606046759cc892e57407633a8e903e7da7d77
Bill Greer <billy.greer@atlanticbiologicals.com>
2019-09-04 15:18:22 -0400

    Initial Commit

---
+++ Notification/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tulpep.NotificationWindow;
+
+namespace Notification
+{
+    public partial class Form1 : Form
+    {
+        System.Threading.Timer timer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+            timer = new System.Threading.Timer(PopUp, null, 1000, 10000);
+            
+        }
+
+        private void PopUp(object state)
+        {
+
+            this.Invoke((MethodInvoker)delegate {
+                // Running on the UI thread
+                PopupNotifier popup = new PopupNotifier();
+                popup.Image = Properties.Resources.info;
+
+                //popup.BodyColor = Color.Yellow;
+                popup.ContentColor = Color.Black;
+                popup.TitleText = "Sales Stats";
+                popup.TitleColor = Color.Red;
+                popup.ContentText = $"Atlantic Biologicals Sales Count: 33 /n Sales Calls: 7";
+
+                popup.Popup();
+            });
+        }
+    }
+}
